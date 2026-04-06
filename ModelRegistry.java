21ba5b85cb4a24d869d73491ae5d0be58dadca07
empiricalcavity3 <bobbynov44@gmail.com>
2026-04-05 22:58:27 -0400

    Delete ModelRegistry.java

--- ModelRegistry.java
+++
@@ -1,7 +0,0 @@
-/**Keeps track of different AI models (e.g., Digit vs Object).
- */
-public class ModelRegistry {
-    private String activeEndpoint = "/predict/general";    public void switchModel(String type) {
-        this.activeEndpoint = "/predict/" + type.toLowerCase();
-    }    public String getEndpoint() { return activeEndpoint; }
-}
